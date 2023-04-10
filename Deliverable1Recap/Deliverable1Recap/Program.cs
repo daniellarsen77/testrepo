3785dc6b4c558fa0d436a2549ccf36698f6318e2
daniellarsen77 <daniellarsen127@gmail.com>
2023-04-10 13:46:44 -0400

    Initial Commit

---
+++ Deliverable1Recap/Deliverable1Recap/Program.cs
@@ -0,0 +1,62 @@
+﻿int sodaCount = 100;
+int sodaRestock = 40;
+
+int chipsCount = 40;
+int chipsRestock = 20;
+
+int candyCount = 60;
+int candyRestock = 40;
+
+Console.WriteLine("How many sodas have been sold? 100 are in stock");
+string sodaSold = Console.ReadLine();
+int s = int.Parse(sodaSold);
+
+if (s > sodaCount)
+{
+    Console.WriteLine("Number to high soda stock not adjusted");
+}
+else
+{
+    sodaCount -= s;
+    Console.WriteLine($"{s} sodas have been sold. {sodaCount} sodas are left in stock");
+}
+
+Console.WriteLine("How many chips have been sold? 40 are in stock");
+string chipsSold = Console.ReadLine();
+int c = int.Parse(chipsSold);
+
+if (c > chipsCount)
+{
+    Console.WriteLine("Number to high chip stock not adjusted");
+}
+else
+{
+    chipsCount -= c;
+    Console.WriteLine($"{c} chips have been sold. {chipsCount} chips are left in stock");
+}
+
+Console.WriteLine("How many candies have been sold? 60 are in stock");
+string candySold = Console.ReadLine();
+int can = int.Parse(candySold);
+
+if (can > candyCount)
+{
+    Console.WriteLine("Number to high candy stock not adjusted");
+}
+else
+{
+    candyCount -= can;
+    Console.WriteLine($"{can} candies have been sold. {candyCount} candies are left in stock");
+}
+
+if (sodaCount <= sodaRestock) { 
+    Console.WriteLine("Sodas need to be restocked");
+}
+if (chipsCount <= chipsRestock)
+{
+    Console.WriteLine("Chips need to be restocked");
+}
+if (candyCount <= candyRestock)
+{
+    Console.WriteLine("Candys need to be restocked");
+}
---
+++ Deliverable2Recap/Deliverable2Recap/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Welcome to the coin flip challenge");
+Console.WriteLine("What is your name");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Well {name}, would you like to do the coin flip challenge? y/n");
+string response = Console.ReadLine();
+
+if (response == "y")
+{
+    //Do the challenge
+    int correctGuesses = 0;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        Random r = new Random();
+        
+        //The minimum number is inclusive 
+        //The maximum number is exclusive
+        //Most random number generators, both numbers are inclusive, not in C#
+        int roll = r.Next(0, 2);
+
+        string result;
+        if (roll == 0)
+        {
+            result = "heads";
+        }
+        else
+        {
+            result = "tails";
+        }
+        Console.WriteLine($"Round number {i}");
+        Console.WriteLine("Guess the flip input heads or tails");
+        string guess = Console.ReadLine();
+
+        if (guess == result) {
+            Console.WriteLine("Correct");
+            correctGuesses ++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect");
+        }
+    }
+        Console.WriteLine($"You got {correctGuesses} out of 5");
+}
+else
+{
+    Console.WriteLine($"You are a coward {name}!");
+}    
